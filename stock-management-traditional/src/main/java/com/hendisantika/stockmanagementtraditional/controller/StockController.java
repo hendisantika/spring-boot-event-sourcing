8514b63224bbd83da089f1651dfa2bdb113c5e54
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-16 10:49:18 +0700

    #7 - Add addStock in StockController in Stock Management in Traditional Way 🚢📗:octocat:🚚

--- stock-management-traditional/src/main/java/com/hendisantika/stockmanagementtraditional/controller/StockController.java
+++ stock-management-traditional/src/main/java/com/hendisantika/stockmanagementtraditional/controller/StockController.java
@@ -1,9 +1,14 @@
 package com.hendisantika.stockmanagementtraditional.controller;
 
+import com.hendisantika.stockmanagementtraditional.entity.Stock;
 import com.hendisantika.stockmanagementtraditional.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +27,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class StockController {
 
     private final StockRepository stockRepository;
+
+    @PostMapping("/stock")
+    public void addStock(@RequestBody Stock stock) {
+        List<Stock> existingStockList = stockRepository.findByName(stock.getName());
+
+        if (existingStockList != null && !existingStockList.isEmpty()) {
+            Stock existingStock = existingStockList.get(0);
+
+            int newQuantity = existingStock.getQuantity() + stock.getQuantity();
+
+            existingStock.setQuantity(newQuantity);
+            existingStock.setAddedBy(stock.getAddedBy());
+            stockRepository.save(existingStock);
+        } else {
+            stockRepository.save(stock);
+        }
+
+    }
 }
