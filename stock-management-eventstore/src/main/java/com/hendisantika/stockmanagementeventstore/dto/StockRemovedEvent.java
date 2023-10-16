3527f985ba75ef5e92e833ca5537ab8f95b9ccba
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-16 11:17:41 +0700

    #17 - Add StockRemovedEvent DTO 🐳📗:octocat:🚛

---
+++ stock-management-eventstore/src/main/java/com/hendisantika/stockmanagementeventstore/dto/StockRemovedEvent.java
@@ -0,0 +1,21 @@
+package com.hendisantika.stockmanagementeventstore.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-event-sourcing
+ * User: hendi
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Link : s.id/hendisantika
+ * Date: 10/16/2023
+ * Time: 11:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Builder
+@Data
+public class StockRemovedEvent implements StockEvent {
+    private Stock stockDetails;
+}
