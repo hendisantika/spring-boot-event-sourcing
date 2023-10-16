584bdf045f254b4b2d7090229b4055c91f5d5bc5
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-16 11:17:08 +0700

    #16 - Add StockAddedEvent DTO 🐳📗:octocat:🚛

---
+++ stock-management-eventstore/src/main/java/com/hendisantika/stockmanagementeventstore/dto/StockEvent.java
@@ -0,0 +1,15 @@
+package com.hendisantika.stockmanagementeventstore.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-event-sourcing
+ * User: hendi
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Link : s.id/hendisantika
+ * Date: 10/16/2023
+ * Time: 11:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface StockEvent {
+}
