e07463d61986ffab721b3eeec8fa5e735308d85f
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-16 11:24:39 +0700

    #27 - Add removeStock in StockController 🐳📗:octocat:🚛

--- stock-management-eventstore/src/main/java/com/hendisantika/stockmanagementeventstore/controller/StockController.java
+++ stock-management-eventstore/src/main/java/com/hendisantika/stockmanagementeventstore/controller/StockController.java
@@ -3,9 +3,11 @@ package com.hendisantika.stockmanagementeventstore.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hendisantika.stockmanagementeventstore.dto.Stock;
 import com.hendisantika.stockmanagementeventstore.dto.StockAddedEvent;
+import com.hendisantika.stockmanagementeventstore.dto.StockRemovedEvent;
 import com.hendisantika.stockmanagementeventstore.service.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,12 @@ public class StockController {
     @PostMapping("/stocks")
     public void addStock(@RequestBody Stock stockRequest) throws JsonProcessingException {
         StockAddedEvent event = StockAddedEvent.builder().stockDetails(stockRequest).build();
+        eventService.addEvent(event);
+    }
+
+    @DeleteMapping("/stocks")
+    public void removeStock(@RequestBody Stock stock) throws JsonProcessingException {
+        StockRemovedEvent event = StockRemovedEvent.builder().stockDetails(stock).build();
         eventService.addEvent(event);
     }
 }
