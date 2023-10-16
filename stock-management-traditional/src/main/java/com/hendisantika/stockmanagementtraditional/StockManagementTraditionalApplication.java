f0e8923193764f0d72a29651dbc797a028461344
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-16 10:41:17 +0700

    #1 - Add Stock Management in Traditional Way 🚢📗:octocat:🚚

---
+++ stock-management-traditional/src/main/java/com/hendisantika/stockmanagementtraditional/StockManagementTraditionalApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.stockmanagementtraditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementTraditionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockManagementTraditionalApplication.class, args);
+    }
+
+}
---
+++ stock-management-traditional/src/test/java/com/hendisantika/stockmanagementtraditional/StockManagementTraditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.stockmanagementtraditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementTraditionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
