b6b55eb296a8a13f9cf20bc0a2d7f5a221a12ae2
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-16 11:18:50 +0700

    #19 - Add findByEntityId in EventRepository DTO 🐳📗:octocat:🚛

--- stock-management-eventstore/src/main/java/com/hendisantika/stockmanagementeventstore/repository/EventRepository.java
+++ stock-management-eventstore/src/main/java/com/hendisantika/stockmanagementeventstore/repository/EventRepository.java
@@ -4,6 +4,8 @@ import com.hendisantika.stockmanagementeventstore.entity.EventStore;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-event-sourcing
@@ -17,5 +19,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public interface EventRepository extends CrudRepository<EventStore, Long> {
+    Iterable<EventStore> findByEntityId(String entityId);
 
+    Iterable<EventStore> findByEntityIdAndEventTimeLessThanEqual(String entityId, LocalDateTime date);
 }
