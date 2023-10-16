50f7b2e12aad881ded5e0545acf936f5ff7cc339
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-16 11:12:25 +0700

    #11 - Add Stock Management Eventstore 🐳📗:octocat:🚛

---
+++ stock-management-eventstore/src/main/java/com/hendisantika/stockmanagementeventstore/StockManagementEventstoreApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.stockmanagementeventstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementEventstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockManagementEventstoreApplication.class, args);
+    }
+
+}
---
+++ stock-management-eventstore/src/test/java/com/hendisantika/stockmanagementeventstore/StockManagementEventstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.stockmanagementeventstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementEventstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
